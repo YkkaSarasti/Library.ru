e3816cd7152fd865cb2268d06da8b7b27320f997
Ykka Sarasti <ykka.sarasti@gmail.com>
2014-03-05 21:19:11 +0400

    Первый коммит
    Как же я задолбался это всё настраивать, но теперь всё работает.

---
+++ catalog.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Библиотека | Каталог</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="icon" href="Image/bookcase_5866.ico">
+        <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+    </head>
+    <body>
+        <header>
+            <div id="logo">Каталог</div>
+            <div id="topmenu">
+                <a href="../index.php"><div class="button">Главная</div></a>
+                <div class="activbutton">Каталог</div>
+                <div class="button">Обратная связь</div>
+                <div class="button">Личный кабинет</div>
+            </div>
+        </header>
+        <nav>
+            <h1>Жанры</h1>
+            <ol>
+                <li>Фантастика</li>
+                <li>Фэнтези</li>
+                <li>Детектив</li>
+            </ol>
+        </nav>
+        <article id="catalog">
+            
+        </article>
+        <footer>
+            &COPY; Все права трын-трава 2014
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Библиотека</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="icon" href="Image/bookcase_5866.ico">
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    </head>
+    <body>
+        <header>
+            <div id="logo">Библиотека</div>
+            <div id="topmenu">
+                <div class="activbutton">Главная</div>
+                <a href="catalog.php"><div class="button">Каталог</div></a>
+                <div class="button">Обратная связь</div>
+                <div class="button">Личный кабинет</div>
+            </div>
+        </header>
+        <article id="main">
+            
+        </article>
+        <footer>
+            &COPY; Все права трын-трава 2014
+        </footer>
+    </body>
+</html>
